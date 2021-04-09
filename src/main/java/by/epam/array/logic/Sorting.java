ad61eb69bfab3eef41686f5f9d0e82eeb0b6df50
Aleksey Malashenkov <alexseymalashenkov@gmail.com>
2021-04-09 12:23:11 +0300

    add method toString to entity class

--- src/main/java/by/epam/array/logic/Sorting.java
+++ src/main/java/by/epam/array/logic/Sorting.java
@@ -1,14 +1,53 @@
 package by.epam.array.logic;
 
+import by.epam.array.entity.ArrayEntity;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Sorting {
     static Logger logger = LogManager.getLogger();
 
-    public void insertionSortArray(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            
+    public void bubbleSort(ArrayEntity array) {
+        logger.info("Start bubble sorting the array");
+
+        int tmp;
+
+        for (int k = array.size() - 1; k > 0; k--) {
+            for (int i = 0; i < k; i++) {
+                if (array.getElement(i) > array.getElement(i + 1)) {
+                    tmp = array.getElement(i);
+                    array.setElement(i, array.getElement(i + 1));
+                    array.setElement(i + 1, tmp);
+                }
+            }
         }
+    }
+
+    public void shakerSort(ArrayEntity array) {
+        logger.info("Start shaker sorting the array");
+
+        int temp;
+        int leftSide = 0;
+        int rightSide = array.size() - 1;
+
+        do {
+            for (int i = leftSide; i < rightSide; i++) {
+                if (array.getElement(i) > array.getElement(i + 1)) {
+                    temp = array.getElement(i);
+                    array.setElement(i, array.getElement(i + 1));
+                    array.setElement(i + 1, temp);
+                }
+            }
+            rightSide--;
+
+            for (int i = rightSide; i > leftSide; i--) {
+                if (array.getElement(i) < array.getElement(i - 1)) {
+                    temp = array.getElement(i);
+                    array.setElement(i, array.getElement(i - 1));
+                    array.setElement(i - 1, temp);
+                }
+            }
+            leftSide++;
+        } while (leftSide < rightSide);
     }
 }
